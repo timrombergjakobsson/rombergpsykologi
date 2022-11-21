808f3abb923bc830bc4e72983811461fda2177bf
Tim <tim.jakobsson@gmail.com>
2022-11-21 15:43:36 +0100

    feat: inital commit with setup

---
+++ portfolio/tailwind.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./index.html"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'mont-serrat': ['"Montserrat"', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
